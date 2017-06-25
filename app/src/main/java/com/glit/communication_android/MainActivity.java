bdc837159c1f6fad73dc1acbfd4e8e6b29c20d6a
Gan Li <baracouta@gmail.com>
2017-06-24 22:11:02 -0500

    Testing GetJson

--- app/src/main/java/com/glit/communication_android/MainActivity.java
+++ app/src/main/java/com/glit/communication_android/MainActivity.java
@@ -1,7 +1,12 @@
 package com.glit.communication_android;
 
+import android.app.ProgressDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.glit.jsontask.GetJsonTask;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button buttonTest = (Button)findViewById(R.id.button_test);
+        final TextView textView = (TextView)findViewById(R.id.textview);
+
+        final GetJsonTask.AsyncResponse response = new GetJsonTask.AsyncResponse() {
+            ProgressDialog progressDialog = new ProgressDialog(MainActivity.this);
+
+            @Override
+            public void processFinish(String output) {
+                progressDialog.setMessage("Downloading");
+                progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+                progressDialog.setIndeterminate(false);
+                progressDialog.show();
+            }
+
+            @Override
+            public void progressUpdate(Integer... values) {
+                textView.setText(output);
+                progressDialog.dismiss();
+            }
+
+            @Override
+            public void preExecute() {
+
+            }
+        }
     }
 }
